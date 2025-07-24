116c42fdfdc621b0e3715c2be7d5532b1062e70f
kaan <darksunnn-kaan@hotmail.com>
2025-07-24 05:22:21 +0200

    tekbiss

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -4,43 +4,21 @@ const nextConfig = {
   poweredByHeader: false,
   images: {
     remotePatterns: [
-      // Local Strapi (development)
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '1337',
         pathname: '/uploads/**',
       },
-      // Production Strapi (domain)
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_STRAPI_BASE_URL?.replace(
+        hostname: process.env.NEXT_PUBLIC_STRAPI_BASE_URL.replace(
           'https://',
           '',
-        ).replace('http://', ''),
-        pathname: '/uploads/**',
-      },
-      // Production Strapi (IP adresi)
-      {
-        protocol: 'http',
-        hostname: '156.67.63.251',
-        port: '1337',
+        ),
         pathname: '/uploads/**',
       },
     ],
-  },
-  // Production'da IP/domain çakışmasını önlemek için
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        'localhost:3000',
-        process.env.NEXT_PUBLIC_STRAPI_BASE_URL?.replace(
-          'https://',
-          '',
-        ).replace('http://', ''),
-        '156.67.63.251:1337',
-      ],
-    },
   },
 };
 
