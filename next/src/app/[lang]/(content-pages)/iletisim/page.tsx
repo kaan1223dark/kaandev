7c9e1ed77eb6507464795652377c0c2fa34098ae
kaan <darksunnn-kaan@hotmail.com>
2026-01-12 02:43:59 +0300

    Sunucu üzerindeki güncellemelers

--- next/src/app/[lang]/(content-pages)/iletisim/page.tsx
+++ next/src/app/[lang]/(content-pages)/iletisim/page.tsx
@@ -1,4 +1,4 @@
-import ContentPage from '@/components/ContentPagenoimage/ContentPage';
+import ContentPage from '@/components/ContentPage/ContentPage';
 import { getDictionary } from '@/dictionaries';
 import { formatMetadata } from '@/libs/strapi/format-metadata';
 import { getStrapiData } from '@/libs/strapi/get-strapi-data';
@@ -7,20 +7,22 @@ import { APIResponse } from '@/types/types';
 import { Metadata } from 'next';
 
 const url =
-  '/api/studies-workshop?populate[0]=Content.banner&populate[1]=Seo.twitter.twitterImage&populate[2]=Seo.openGraph.openGraphImage&populate[3]=ContactBanner';
-const tags = ['studies-workshop'];
+  '/api/contact?populate[0]=Content.banner&populate[1]=Seo.twitter.twitterImage&populate[2]=Seo.openGraph.openGraphImage';
+const tags = ['contact'];
 
-interface StudiesWorkshopProps {
+interface ContactProps {
   params: Promise<{ lang: SupportedLanguage }>;
 }
 
-export default async function StudiesWorkshop(props: StudiesWorkshopProps) {
+export default async function Contact(props: ContactProps) {
   const params = await props.params;
   const dictionary = await getDictionary(params.lang);
 
-  const pageData = await getStrapiData<
-    APIResponse<'api::studies-workshop.studies-workshop'>
-  >(params.lang, url, tags);
+  const pageData = await getStrapiData<APIResponse<'api::contact.contact'>>(
+    params.lang,
+    url,
+    tags,
+  );
 
   return (
     <ContentPage
@@ -31,15 +33,15 @@ export default async function StudiesWorkshop(props: StudiesWorkshopProps) {
   );
 }
 
-export async function generateMetadata(
-  props: StudiesWorkshopProps,
-): Promise<Metadata> {
+export async function generateMetadata(props: ContactProps): Promise<Metadata> {
   const params = await props.params;
-  const data = await getStrapiData<
-    APIResponse<'api::studies-workshop.studies-workshop'>
-  >(params.lang, url, tags);
+  const data = await getStrapiData<APIResponse<'api::contact.contact'>>(
+    params.lang,
+    url,
+    tags,
+  );
 
-  const pathname = `/${params.lang}/studies/workshops`;
+  const pathname = `/${params.lang}/contact`;
 
   return formatMetadata(data, pathname);
 }
